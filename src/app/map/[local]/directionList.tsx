812726f44fbb39c2a6439ad9d74dea2a8ff9618f
AnYoonKyoung <ank04250425@gmail.com>
2024-07-15 18:23:40 +0900

    feat(@main):구글 길찾기 api 추가

--- src/app/map/[local]/directionList.tsx
+++ src/app/map/[local]/directionList.tsx
@@ -3,26 +3,66 @@ import SearchBar from "../../../components/filterbar/search";
 import SelectFilter from "../../../components/filterbar/fieldSelect";
 import Image from "next/image";
 import { useRecoilState } from "recoil";
-import { textState } from "@/components/atoms";
-import React, { useState, useEffect } from "react";
 
+import React, { useState, useEffect } from "react";
+import { FaTrashCan } from "react-icons/fa6";
 import Chip from "@mui/material/Chip";
+import { textState } from "@/components/atoms";
+import { useRecoilValue } from "recoil";
 import Button from "@mui/material/Button";
 
-const DirectionList = ({ mapArray }) => {
+const DirectionList = () => {
   const [filterChip, setFilterChip] = useState("loc");
+  const [selectList, setSelectList] = useRecoilState(textState);
+  const [todoList, setTodoList] = useRecoilState(textState);
+  const markerPositions = useRecoilValue<any[]>(textState);
+  const [directions, setDirections] = useState(null);
+  const [error, setError] = useState(null);
+  const filterArray = [
+    { name: "자동차", code: "drive" },
+    { name: "버스", code: "bus" },
+  ];
+  const origin = `${markerPositions[0].y},${markerPositions[0].x}`;
+  const destination = `${markerPositions[markerPositions.length - 1].y},${
+    markerPositions[markerPositions.length - 1].x
+  }`;
+  const waypoints = markerPositions
+    .slice(1, -1)
+    .map((wp) => `${wp.y},${wp.x}`)
+    .join("|");
+  console.log(origin, destination, waypoints);
+  console.log(todoList);
+  useEffect(() => {
+    const url = `/api/dir?origin=${origin}&destination=${destination}`;
+    const fetchDirections = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (res.ok) {
+          setDirections(data);
+        } else {
+          setError(data.error);
+        }
+      } catch (err) {
+        setError("Failed to fetch directions");
+      }
+    };
+
+    fetchDirections();
+  }, []);
+
   return (
-    <>
+    <div>
       <SearchBar />
-      <SelectFilter setFilterChip={setFilterChip} />
-      {mapArray.map((el) => (
+      <SelectFilter setFilterChip={setFilterChip} Array={filterArray} />
+      {todoList.map((el) => (
         <div key={el.id} className="rounded-xl p-10 flex flex-col border-b ">
           <div
             className="text-3xl h-10 flex-row mb-3"
             // onClick={() => toggleCollapse(el.place_url)}
           >
             {el.place_name}
-            <Chip label={el.category_group_name} />
+            {/* <Chip label={el.category_group_name} /> */}
           </div>
           <div className=" flex flex-row ">
             <Image
@@ -37,37 +77,25 @@ const DirectionList = ({ mapArray }) => {
 
             <div className="flex ml-10 flex-row justify-between w-full">
               <div>
-                <span className="flex m-0 p-0 flex-row  flex-wrap">
-                  {el?.category_name?.split(" > ").map((category) => (
-                    <Chip
-                      key={category}
-                      label={category}
-                      size="small"
-                      variant="Filled"
-                      className="m-1"
-                    />
-                  ))}
-                </span>
-
-                <div>
-                  {/* <span onClick={toggleCollapse}>{el.tag}</span> */}
-                  {el.road_address_name}
-                </div>
+                {/* <div>
+                      <span onClick={toggleCollapse}>{el.tag}</span>
+                      {el.road_address_name}
+                    </div> */}
               </div>
               <div>
                 <Button
                   variant="contained"
                   className="h-full"
-                  // onClick={() => locationHanlder(el.x, el.y)}
+                  onClick={() => locationHanlder(el.x, el.y, el.place_name)}
                 >
-                  +
+                  <FaTrashCan />
                 </Button>
               </div>
             </div>
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
---
+++ src/pages/api/dir.tsx
@@ -0,0 +1,28 @@
+// pages/api/directions.js
+
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { origin, destination, waypoints } = req.query;
+
+  console.log(origin, destination, waypoints);
+  if (!origin || !destination) {
+    return res
+      .status(400)
+      .json({ error: "Origin and destination are required" });
+  }
+
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_KEY;
+  let url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&travelMode='DRIVING`;
+  let urls = `https://maps.googleapis.com/maps/api/directions/json?origin=%EC%84%9C%EC%9A%B8%20%EA%B0%95%EB%82%A8%EA%B5%AC%20%EB%86%80%ED%98%84%EB%A1%9C%20508&destination=%EC%84%9C%EC%9A%B8%20%EC%86%A1%ED%8C%8C%EA%B5%AC%20%EC%98%AC%EB%A6%BC%ED%94%BC%EB%A1%9C%20300&key=${apiKey}
+`;
+  // if (waypoints && waypoints.length > 0) {
+  //   url += `&waypoints=${waypoints}`;
+  // }
+  try {
+    const response = await axios.get(urls);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
