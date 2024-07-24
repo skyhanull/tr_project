8cbb1dca39cc0ccdfa2ed5b073e9e43303c77cae
AnYoonKyoung <anky0425@naver.com>
2024-07-25 02:52:17 +0900

    fix(@main):필요없는 주석 및 코드 삭제

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,8 @@
-import Search from "../../components/filterbar/search";
 import Carousel from "@/components/caresel";
 
 export default function Page() {
   return (
     <div className="justify-center mr-60 ml-60">
-      <Search />
       <Carousel />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
+import Button from "@mui/material/Button";
+export default function Main() {
+  const route = useRouter();
 
-export default function Home() {
+  const ClickHandler = () => {
+    route.push("/about");
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>a</div>
+      <div className="justify-center flex">
+        <Button
+          disableElevation
+          className="bg-customPink text-5xl font-blod p-10 w-96 rounded-full mt-52 text-black "
+          onClick={ClickHandler}
+        >
+          시작하기
+        </Button>
+      </div>
     </main>
   );
 }
