082a78ad807964c6efa6aff67a798c16c5f48faa
AnYoonKyoung <anky0425@naver.com>
2024-07-24 23:37:55 +0900

    fix(@main):네비게이션추가

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,16 +1,25 @@
+"use client";
 import Image from "next/image";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+
+  const LogoHanlder = () => {
+    router.push("/");
+  };
   return (
     <header>
-      <div className="flex justify-between items-center m-8">
+      <div
+        className="flex justify-between items-center m-8"
+        onClick={LogoHanlder}
+      >
         <div className="flex items-center space-x-4">
           <Image src="/icon/Vector.png" alt="Logo" width={50} height={70} />
           <Image src="/img/logo.png" alt="Logo" width={100} height={70} />
         </div>
 
         <div className="flex items-center space-x-4">
-          <span className="text-3xl p-7">gtp연결</span>
           <span className="text-3xl p-7">gtp연결</span>
 
           <span className="text-3xl p-7">로그인</span>
