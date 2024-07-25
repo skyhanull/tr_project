9745cd450a05d91b56a70c91f06f49f89ad085d6
AnYoonKyoung <anky0425@naver.com>
2024-07-26 05:05:00 +0900

    fix(@main):길찾기 탭 구조 수정

--- src/app/map/[local]/directionList.tsx
+++ src/app/map/[local]/directionList.tsx
@@ -1,53 +1,47 @@
 "use client";
-import SearchBar from "../../../components/filterbar/search";
-import SelectFilter from "../../../components/filterbar/fieldSelect";
-import Image from "next/image";
-import { useRecoilState } from "recoil";
-import convertDuration from "@/util/time";
 import React, { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import Image from "next/image";
 import { FaTrashCan } from "react-icons/fa6";
-import Chip from "@mui/material/Chip";
 import { textState } from "@/components/atoms";
-import { useRecoilValue } from "recoil";
+import { FaShareNodes } from "react-icons/fa6";
+import SelectFilter from "../../../components/filterbar/fieldSelect";
+import convertDuration from "@/util/time";
 import Button from "@mui/material/Button";
 import DirectionModal from "@/components/directionModal";
-import getChipColor from "@/util/color";
+import DistanceM from "@/util/distance";
 import getImageSrc from "@/util/image";
+import ShareKakao from "@/components/shareKakao";
+const filterArray = [
+  { name: "자동차", code: "driving" },
+  { name: "도보", code: "walking" },
+  { name: "실시간 경로", code: "driving-traffic" },
+];
+
 const DirectionList = () => {
-  const [filterChip, setFilterChip] = useState("loc");
-  // const [selectList, setSelectList] = useRecoilState(textState);
-  // const [todoList, setTodoList] = useRecoilState(textState);
+  const [filterChip, setFilterChip] = useState("driving");
   const [markerList, setMarkerList] = useRecoilState(textState);
   const [directions, setDirections] = useState(null);
   const [isCollapsed, setIsCollapsed] = useState(false);
-  // const backgroundColorClass = getChipColor(markerList);
   const [error, setError] = useState(null);
-  const filterArray = [
-    { name: "자동차", code: "drive" },
-    { name: "버스", code: "bus" },
-  ];
 
   const destination = `${markerList[markerList.length - 1]?.x},${
     markerList[markerList.length - 1]?.y
   }`;
 
-  const AAA = markerList
+  const origin = markerList
     ?.map((el) => `${el.x},${el.y}`)
     .slice(0, -1)
     .join(";");
 
-  // console.log(markerPositions?.map((el) => `${el.x},${el.y}`).join(";"));
-  // console.log(origin, destination, waypoints);
-  console.log(AAA);
   useEffect(() => {
-    const url = `/api/mapbox?origin=${AAA}&destination=${destination}`;
+    const url = `/api/mapbox?origin=${origin}&destination=${destination}&mode=${filterChip}`;
     const fetchDirections = async () => {
       try {
         const res = await fetch(url);
         const data = await res.json();
         if (res.ok) {
           setDirections(data);
-          console.log(data);
         } else {
           setError(data.error);
         }
@@ -59,70 +53,95 @@ const DirectionList = () => {
     if (markerList) {
       fetchDirections();
     }
-  }, [markerList]);
+  }, [markerList, filterChip]);
 
   const deleteFilter = (i: number) => {
     const newList = markerList.filter((el, index) => index !== i);
     setMarkerList(newList);
   };
-  console.log(markerList);
+
   return (
     <>
       <div>
-        <div>
+        <div className="flex items-center justify-between mr-5 mt-5">
           <div className="text-4xl m-8">길찾기</div>
-          <button onClick={() => setIsCollapsed(true)}>상세보기</button>
+          <div className="flex items-center">
+            <button
+              onClick={() => setIsCollapsed(true)}
+              className="bg-pink-300 p-3 mr-4 rounded-xl text-white"
+            >
+              상세보기
+            </button>
+            <ShareKakao
+              title="제목 예시"
+              description="설명 예시"
+              imageUrl="https://example.com/image.jpg"
+              linkUrl="https://example.com"
+            />
+          </div>
         </div>
 
         <SelectFilter setFilterChip={setFilterChip} Array={filterArray} />
-
-        {markerList.length === 0 ? (
-          <div>no result</div>
-        ) : (
-          markerList.map((el, i) => (
-            <div
-              key={el.id}
-              className="rounded-xl p-10 flex flex-col border-b "
-            >
-              <div className=" flex flex-row ">
-                <Image
-                  src={getImageSrc(el.filterChip)}
-                  alt=""
-                  layout="fixed"
-                  width={100}
-                  height={100}
-                  objectFit="cover" // 원본 이미지의 비율을 유지하면서 컨테이너에 맞게 조정
-                  className="rounded-lg shadow-md"
-                />
-                <div className="flex ml-10 flex-row justify-between w-full">
-                  <div>
+        <div className="h-full">
+          {markerList.length < 2 ? (
+            <div>no result</div>
+          ) : (
+            markerList.map((el, i) => (
+              <div
+                key={el.id}
+                className="rounded-xl p-8 flex flex-col border-b "
+              >
+                <div className=" flex flex-row ">
+                  <Image
+                    src={getImageSrc(el.filterChip)}
+                    alt=""
+                    layout="fixed"
+                    width={100}
+                    height={100}
+                    objectFit="cover" // 원본 이미지의 비율을 유지하면서 컨테이너에 맞게 조정
+                    className="rounded-lg shadow-md"
+                  />
+                  <div className="flex ml-10 flex-row justify-between w-full">
                     <div>
-                      {/* <span onClick={toggleCollapse}>{el.tag}</span> */}
-                      {el.name}
+                      <div>
+                        {/* <span onClick={toggleCollapse}>{el.tag}</span> */}
+                        {el.name}
+                      </div>
+                      <div>{el.filterChip}</div>
+                      <div>{el.address}</div>
                     </div>
-                    <div>{el.filterChip}</div>
-                    <div>{el.address}</div>
-                  </div>
-                  <div>
-                    <Button
-                      variant="contained"
-                      className="h-full bg-red-500"
-                      onClick={() => deleteFilter(i)}
-                    >
-                      <FaTrashCan />
-                    </Button>
+                    <div>
+                      <Button
+                        variant="contained"
+                        className="h-full bg-red-500"
+                        onClick={() => deleteFilter(i)}
+                      >
+                        <FaTrashCan />
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
+            ))
+          )}
+          <div className="border-y-2  fixed bottom-0 p-10 flex flex-col bg-white w-px-55">
+            <div className="text-2xl flex justify-between w-full">
+              <div className="font-bold">총 시간 :</div>
+              <div className="flex ml-48">
+                {directions?.routes[0]?.duration
+                  ? convertDuration(directions?.routes[0]?.duration)
+                  : "0초"}
+              </div>
             </div>
-          ))
-        )}
-        <div className="h-60 border-2">
-          <div>
-            총 시간 : {convertDuration(directions?.routes[0]?.duration)}
+            <div className="text-2xl flex justify-between w-full">
+              <div className="font-bold">총 거리 :</div>
+              <div className="flex">
+                {directions?.routes[0]?.distance
+                  ? DistanceM(directions?.routes[0]?.distance)
+                  : "0km"}
+              </div>
+            </div>
           </div>
-          <div>총 거리 : {directions?.routes[0]?.distance}</div>
-          <div>거리:</div>
         </div>
       </div>
       {isCollapsed && (
