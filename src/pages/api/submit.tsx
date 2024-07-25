035c0e4c3463cc122d2325cef49196cbe7a96bcb
AnYoonKyoung <anky0425@naver.com>
2024-07-26 05:06:50 +0900

    fix(@main):페이지 삭제

--- src/pages/api/submit.tsx
+++
@@ -1,9 +0,0 @@
-// pages/api/hello.js
-
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ message: "Hello, Next.js 14 API!" });
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
-}
