96e6b8f8ac381b818e500eb343f86eb4ac31ad75
AnYoonKyoung <anky0425@naver.com>
2024-07-25 23:40:36 +0900

    fix(@main):장소 리스트 수정

--- src/app/map/[local]/locationList.tsx
+++ src/app/map/[local]/locationList.tsx
@@ -2,12 +2,11 @@
 import Image from "next/image";
 import { useRecoilState } from "recoil";
 import React, { useState, useEffect } from "react";
-import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { textState } from "@/components/atoms";
 import { HiOutlinePlus } from "react-icons/hi";
 import { Place } from "@/util/interface/listInterface";
 import { searchPlaces } from "../../../util/kakao";
-import InfiniteScroll from "react-infinite-scroll-component";
 import SelectFilter from "../../../components/filterbar/fieldSelect";
 import SearchBar from "../../../components/filterbar/search";
 import ListModal from "../../../components/listModal";
@@ -16,11 +15,14 @@ import Pagination from "@mui/material/Pagination";
 import Button from "@mui/material/Button";
 import getChipColor from "@/util/color";
 import getImageSrc from "@/util/image";
-export enum Status {
-  MAP = 1,
-  SUCCESS = 2,
-  ERROR = "error",
-}
+
+const filterArray = [
+  { name: "추천루트", code: "loc" },
+  { name: "맛집", code: "FD6" },
+  { name: "관광지", code: "AT4" },
+  { name: "카페", code: "CE7" },
+  { name: "축제", code: "fes" },
+];
 
 const LocationList = () => {
   const router = usePathname();
@@ -29,18 +31,12 @@ const LocationList = () => {
   const [filterChip, setFilterChip] = useState("loc");
   const [mapArray, setMapArray] = useState<Place[]>([]);
   const [curUrl, setCurUrl] = useState("");
-  const [hasMore, setHasMore] = useState(true);
+
   const [currentPage, setCurrentPage] = useState(1);
   const backgroundColorClass = getChipColor(filterChip);
   const [totalPages, setTotalPages] = useState(0);
-  const filterArray = [
-    { name: "추천루트", code: "loc" },
-    { name: "맛집", code: "FD6" },
-    { name: "관광지", code: "AT4" },
-    { name: "카페", code: "CE7" },
-    { name: "축제", code: "fes" },
-  ];
-  //몰고디비 연결 api
+
+  //몽고디비 연결 api
   const fetchData = async (collection = "") => {
     const url = collection
       ? `/api/mapArray?collectionName=${
@@ -50,21 +46,19 @@ const LocationList = () => {
     const res = await fetch(url);
     const data = await res.json();
     setMapArray(data); // Adjust according to your API response
-    // setTotalPages(data.totalPages); // Adjust according to your API response
   };
+
   //카카오 장소 검색 연결 api
   const fetchPlaces = async () => {
     try {
       const options = {
         size: 10, // 한 페이지에 보여질 문서의 개수
         page: currentPage,
-        // sort: "accuracy",
         category_group_code: filterChip,
       };
-      const results = await searchPlaces("서울", options);
+      const results = await searchPlaces(router?.split("/")[2], options);
       setTotalPages(Math.ceil(results.meta.total_count / 10));
       setMapArray(results.documents);
-      // setTotalPages(results.totalPages);
     } catch (error) {
       console.error("Error fetching places:", error);
     }
@@ -91,82 +85,91 @@ const LocationList = () => {
   const handlePageChange = (event, page) => {
     setCurrentPage(page);
   };
+
   return (
     <>
-      <div className="mb-40">
+      <div className="mb-40 mt-30">
         <SearchBar />
         <SelectFilter setFilterChip={setFilterChip} Array={filterArray} />
 
-        {mapArray.map((el) => (
-          <div
-            key={el.id}
-            className="h-full rounded-xl p-10 flex flex-col border-b overflow-auto"
-          >
-            <div
-              className="h-10 flex-row mb-3 text-3xl "
-              onClick={() => toggleCollapse(el.place_url)}
-            >
-              {el.place_name}
-              <Chip
-                label={el.category_group_name}
-                sx={{ backgroundColorClass, color: backgroundColorClass }}
-                variant="outlined"
-                className="ml-3"
-              />
-            </div>
-            <div className=" flex flex-row ">
-              <Image
-                src={getImageSrc(filterChip)}
-                alt=""
-                layout="fixed"
-                width={100}
-                height={100}
-                objectFit="cover" // 원본 이미지의 비율을 유지하면서 컨테이너에 맞게 조정
-                className="rounded-lg shadow-md"
-              />
-
-              <div className="flex ml-8 flex-row justify-between w-full">
-                <div>
-                  <span className="flex m-0 p-0 flex-row  flex-wrap">
-                    {el?.category_name?.split(" > ").map((category) => (
-                      <Chip
-                        key={category}
-                        label={category}
-                        size="small"
-                        variant="Filled"
-                        className="m-1"
-                      />
-                    ))}
-                  </span>
-
-                  <div className="m-1 mt-2">{el.road_address_name}</div>
+        {mapArray.length === 0 ? (
+          <div className="flex justify-center items-center">
+            데이터가 없습니다
+          </div>
+        ) : (
+          <>
+            {mapArray.map((el) => (
+              <div
+                key={el.id}
+                className="h-full rounded-xl p-10 flex flex-col border-b overflow-auto"
+              >
+                <div
+                  className="h-10 flex-row mb-3 text-3xl "
+                  onClick={() => toggleCollapse(el.place_url)}
+                >
+                  {el.place_name}
+                  <Chip
+                    label={el.category_group_name}
+                    sx={{ backgroundColorClass, color: backgroundColorClass }}
+                    variant="outlined"
+                    className="ml-3"
+                  />
                 </div>
-                <div>
-                  <Button
-                    // variant="contained"
-                    className="h-full bg-red-100 text-zinc-700"
-                    onClick={() =>
-                      locationHanlder(
-                        el.x,
-                        el.y,
-                        el.place_name,
-                        el.road_address_name
-                      )
-                    }
-                  >
-                    <HiOutlinePlus className="text-2xl" />
-                  </Button>
+                <div className=" flex flex-row ">
+                  <Image
+                    src={getImageSrc(filterChip)}
+                    alt=""
+                    layout="fixed"
+                    width={100}
+                    height={100}
+                    objectFit="cover" // 원본 이미지의 비율을 유지하면서 컨테이너에 맞게 조정
+                    className="rounded-lg shadow-md"
+                  />
+
+                  <div className="flex ml-8 flex-row justify-between w-full">
+                    <div>
+                      <span className="flex m-0 p-0 flex-row  flex-wrap">
+                        {el?.category_name?.split(" > ").map((category, i) => (
+                          <Chip
+                            key={i}
+                            label={category}
+                            size="small"
+                            variant="Filled"
+                            className="m-1"
+                          />
+                        ))}
+                      </span>
+
+                      <div className="m-1 mt-2">{el.road_address_name}</div>
+                    </div>
+                    <div>
+                      <Button
+                        // variant="contained"
+                        className="h-full bg-red-100 text-zinc-700"
+                        onClick={() =>
+                          locationHanlder(
+                            el.x,
+                            el.y,
+                            el.place_name,
+                            el.road_address_name
+                          )
+                        }
+                      >
+                        <HiOutlinePlus className="text-2xl" />
+                      </Button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
         <Pagination
           count={totalPages}
           page={currentPage}
           onChange={handlePageChange}
           variant="outlined"
-          color="secondary"
+          className="p-6 flex justify-center text-pink-500"
         />
       </div>
       {isCollapsed && (
