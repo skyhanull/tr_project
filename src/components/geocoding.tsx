5babd3fc086610377f48e9d1a80eddc864a4bce5
AnYoonKyoung <anky0425@naver.com>
2024-07-26 05:13:51 +0900

    fix(@main):사용하지 않는 api수정

---
+++ src/components/geocoding.tsx
@@ -0,0 +1,14 @@
+// // components/Geocoder.js
+// import { useEffect } from "react";
+
+// const Geocoder = ({ address, onGeocode }) => {
+//   var geocoder = new kakao.maps.services.Geocoder();
+
+//   var callback = function (result, status) {
+//     if (status === kakao.maps.services.Status.OK) {
+//       console.log(result);
+//     }
+//   };
+// };
+
+// export default Geocoder;
