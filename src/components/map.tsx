381c548ec7ece27a77059a34604e519bb06febc2
AnYoonKyoung <anky0425@naver.com>
2024-07-25 23:41:13 +0900

    fix(@main):사용하지않는 api 주석처리

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,93 +1,95 @@
-"use client";
+// "use client";
 
-import React, { useEffect, useRef } from "react";
-import { textState } from "@/components/atoms";
-import { useRecoilValue } from "recoil";
-import axios from "axios";
+// import React, { useEffect, useRef } from "react";
+// import { textState } from "@/components/atoms";
+// import { useRecoilValue } from "recoil";
+// import axios from "axios";
 
-declare global {
-  interface Window {
-    naver: any;
-  }
-}
+// declare global {
+//   interface Window {
+//     naver: any;
+//   }
+// }
 
-const Map = () => {
-  const mapElement = useRef(null);
-  const markerPositions = useRecoilValue(textState);
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=YOUR_CLIENT_ID`;
-    script.onload = () => {
-      const { naver } = window;
-      if (!mapElement.current || !naver) return;
+// const Map = () => {
+//   const mapElement = useRef(null);
+//   const markerPositions = useRecoilValue(textState);
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=YOUR_CLIENT_ID`;
+//     script.onload = () => {
+//       const { naver } = window;
+//       if (!mapElement.current || !naver) return;
 
-      const mapOptions = {
-        center: new naver.maps.LatLng(37.5665, 126.978), // 초기 중심 좌표 (서울 시청)
-        zoom: 10,
-      };
+//       const mapOptions = {
+//         center: new naver.maps.LatLng(37.5665, 126.978), // 초기 중심 좌표 (서울 시청)
+//         zoom: 10,
+//       };
 
-      const map = new naver.maps.Map(mapElement.current, mapOptions);
+//       const map = new naver.maps.Map(mapElement.current, mapOptions);
 
-      // 추가적인 지도 설정 및 마커 추가 코드
-      markerPositions.forEach((position) => {
-        new naver.maps.Marker({
-          position: new naver.maps.LatLng(position.y, position.x),
-          map,
-        });
-      });
-      if (markerPositions.length > 1) {
-        const path = markerPositions.map(
-          (position) => new naver.maps.LatLng(position.y, position.x)
-        );
-        const polyline = new naver.maps.Polyline({
-          map: map,
-          path: path,
-          strokeColor: "#5347AA",
-          strokeWeight: 3,
-        });
-      }
-      // 길찾기 경로 추가
-      // 길찾기 경로 추가
-      if (markerPositions.length > 1) {
-        const fetchDirections = async () => {
-          const start = markerPositions[0];
-          const end = markerPositions[1];
-          try {
-            const response = await fetch(
-              `/api/directions?start=${start.x},${start.y}&goal=${end.x},${end.y}`
-            );
-            const data = await response.json();
-            const path = data.route.traoptimal[0].path.map(
-              ([lng, lat]) => new naver.maps.LatLng(lat, lng)
-            );
-            new naver.maps.Polyline({
-              map: map,
-              path: path,
-              strokeColor: "#5347AA",
-              strokeWeight: 3,
-            });
-            const summary = data.route.traoptimal[0].summary;
-            console.log(
-              `총 거리: ${summary.distance}m, 예상 시간: ${summary.duration}ms, 교통 수단: ${summary.fare}`
-            );
-          } catch (error) {
-            console.error("Failed to fetch directions:", error);
-          }
-        };
+//       // 추가적인 지도 설정 및 마커 추가 코드
+//       markerPositions.forEach((position) => {
+//         new naver.maps.Marker({
+//           position: new naver.maps.LatLng(position.y, position.x),
+//           map,
+//         });
+//       });
+//       if (markerPositions.length > 1) {
+//         const path = markerPositions.map(
+//           (position) => new naver.maps.LatLng(position.y, position.x)
+//         );
+//         const polyline = new naver.maps.Polyline({
+//           map: map,
+//           path: path,
+//           strokeColor: "#5347AA",
+//           strokeWeight: 3,
+//         });
+//       }
+//       // 길찾기 경로 추가
+//       // 길찾기 경로 추가
+//       if (markerPositions.length > 1) {
+//         const fetchDirections = async () => {
+//           const start = markerPositions[0];
+//           const end = markerPositions[1];
+//           try {
+//             const response = await fetch(
+//               `/api/directions?start=${start.x},${start.y}&goal=${end.x},${end.y}`
+//             );
+//             const data = await response.json();
+//             const path = data.route.traoptimal[0].path.map(
+//               ([lng, lat]) => new naver.maps.LatLng(lat, lng)
+//             );
+//             new naver.maps.Polyline({
+//               map: map,
+//               path: path,
+//               strokeColor: "#5347AA",
+//               strokeWeight: 3,
+//             });
+//             const summary = data.route.traoptimal[0].summary;
+//             console.log(
+//               `총 거리: ${summary.distance}m, 예상 시간: ${summary.duration}ms, 교통 수단: ${summary.fare}`
+//             );
+//           } catch (error) {
+//             console.error("Failed to fetch directions:", error);
+//           }
+//         };
 
-        fetchDirections();
-      }
-    };
-    // 경로 추가
+//         fetchDirections();
+//       }
+//     };
+//     // 경로 추가
 
-    document.head.appendChild(script);
+//     document.head.appendChild(script);
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, [markerPositions]);
+//     return () => {
+//       document.head.removeChild(script);
+//     };
+//   }, [markerPositions]);
 
-  return <div ref={mapElement} style={{ width: "100%", height: "100%" }} />;
-};
+//   return <div ref={mapElement} style={{ width: "100%", height: "100%" }} />;
+// };
 
-export default Map;
+// export default Map;
+
+//네이버 길찾기 예시
--- src/pages/api/naverSearch.tsx
+++ src/pages/api/naverSearch.tsx
@@ -1,55 +1,57 @@
-// src/pages/api/naverSearch.ts
+// // src/pages/api/naverSearch.ts
 
-import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import axios from "axios";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { query, page = 1 } = req.query;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { query, page = 1 } = req.query;
 
-  if (!query) {
-    return res.status(400).json({ error: "Query parameter is required" });
-  }
+//   if (!query) {
+//     return res.status(400).json({ error: "Query parameter is required" });
+//   }
 
-  const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_OPEN_ID;
-  const clientSecret = process.env.NEXT_PUBLIC_NAVER_CLIENT_OPEN_SECRET;
+//   const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_OPEN_ID;
+//   const clientSecret = process.env.NEXT_PUBLIC_NAVER_CLIENT_OPEN_SECRET;
 
-  if (!clientId || !clientSecret) {
-    console.error("API keys are missing");
-    return res.status(500).json({ error: "API keys are missing" });
-  }
+//   if (!clientId || !clientSecret) {
+//     console.error("API keys are missing");
+//     return res.status(500).json({ error: "API keys are missing" });
+//   }
 
-  const url = "https://openapi.naver.com/v1/search/local.json";
-  const encodedQuery = query; // 검색어를 URL 인코딩
-  const itemsPerPage = 100;
-  const start = (Number(page) - 1) * itemsPerPage + 1; // 시작 인덱스 계산
-  const params = {
-    query: encodedQuery,
-    display: 100,
-    start,
-  };
-  const headers = {
-    "X-Naver-Client-Id": clientId,
-    "X-Naver-Client-Secret": clientSecret,
-  };
+//   const url = "https://openapi.naver.com/v1/search/local.json";
+//   const encodedQuery = query; // 검색어를 URL 인코딩
+//   const itemsPerPage = 100;
+//   const start = (Number(page) - 1) * itemsPerPage + 1; // 시작 인덱스 계산
+//   const params = {
+//     query: encodedQuery,
+//     display: 100,
+//     start,
+//   };
+//   const headers = {
+//     "X-Naver-Client-Id": clientId,
+//     "X-Naver-Client-Secret": clientSecret,
+//   };
 
-  try {
-    console.log("Requesting Naver API with:", params);
+//   try {
+//     console.log("Requesting Naver API with:", params);
 
-    const response = await axios.get(url, {
-      params,
-      headers,
-    });
-    console.log("Naver API response:", response.data);
-    console.log("Encoded Query:", encodedQuery);
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error(
-      "Error fetching data from Naver API:",
-      error.response?.data || error.message
-    );
-    res.status(500).json({ error: error.response.data });
-  }
-}
+//     const response = await axios.get(url, {
+//       params,
+//       headers,
+//     });
+//     console.log("Naver API response:", response.data);
+//     console.log("Encoded Query:", encodedQuery);
+//     res.status(200).json(response.data);
+//   } catch (error) {
+//     console.error(
+//       "Error fetching data from Naver API:",
+//       error.response?.data || error.message
+//     );
+//     res.status(500).json({ error: error.response.data });
+//   }
+// }
+
+//이 부분은 네이버주변검색을 이용하려했지만 5개밖에 나오지않아 이용 x
