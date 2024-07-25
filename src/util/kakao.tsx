55f3d807f98ff68ff9d64cd118f0cb9890d7621a
AnYoonKyoung <anky0425@naver.com>
2024-07-26 05:06:27 +0900

    feat(@main):장소검색 컴포넌트 생성

---
+++ src/util/kakao.tsx
@@ -0,0 +1,27 @@
+export async function searchPlaces(query, options = {}) {
+  const apiKey = process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY;
+  let url = `https://dapi.kakao.com/v2/local/search/keyword.json?query=${query}`;
+  if (options.category_group_code) {
+    url += `&category_group_code=${options.category_group_code}`;
+  }
+  if (options.page) {
+    url += `&page=${options.page}`;
+  }
+  if (options.size) {
+    url += `&size=${options.size}`;
+  }
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `KakaoAK ${apiKey}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
