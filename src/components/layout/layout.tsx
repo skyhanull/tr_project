c6cd00d28e9b877fd98de85c2edb40735e387c12
AnYoonKyoung <anky0425@naver.com>
2024-07-28 01:08:22 +0900

    fix(@main):타입 수정

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,12 @@
+import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode; // Define the type for children
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
