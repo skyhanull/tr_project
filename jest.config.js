411ddbdcdd0f5dc94b3846c33bc2f8327a3fb1fd
AnYoonKyoung <anky0425@naver.com>
2024-07-30 17:52:38 +0900

    feat(@main):jest 및 testing라이브러리 설치

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
