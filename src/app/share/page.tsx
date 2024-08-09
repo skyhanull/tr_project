1bbe896402faf898d8f21df0aa7102232714f7ab
AnYoonKyoung <anky0425@naver.com>
2024-08-09 23:22:28 +0900

    refactor(develop):페이지네이션 추가 및 로딩 상태 추가

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,24 +1,39 @@
 "use client";
-import axios from "axios";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import Card from "../../components/Card/card";
 import { storeListHandler } from "../../hook/storeListHandler";
+import Pagination from "@mui/material/Pagination";
+
 export default function Page() {
-  const userCode = localStorage.getItem(null);
   const [cardData, setCardData] = useState([]);
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
+  const [pagination, setPagination] = useState({
+    total: 0,
+    page: 1,
+    limit: 10,
+    totalPages: 0,
+  });
+
   useEffect(() => {
-    // Call storeListHandler regardless of userCode presence
-    storeListHandler(userCode || null, setCardData);
-  }, []);
+    const fetchData = async () => {
+      setLoading(true); // 데이터를 가져오기 시작할 때 로딩 상태로 설정
+      await storeListHandler(null, setCardData, setPagination, pagination);
+      setLoading(false); // 데이터 가져오기 완료 후 로딩 상태 해제
+    };
+
+    fetchData();
+  }, [pagination.page]); // 페이지 변경 시마다 데이터 다시 가져오기
 
   return (
-    <div className="justify-center  mx-20 mt-32">
-      <div className="flex text-3xl">my tour List</div>
-
-      <div className="h-screen border-2 border-gray-300">
+    <div className="justify-center mx-20 mt-32">
+      <div className="h-screen">
         <div>
-          <Card cardData={cardData} />
+          <Card
+            cardData={cardData}
+            setPagination={setPagination}
+            pagination={pagination}
+            loading={loading} // 로딩 상태 전달
+          />
         </div>
       </div>
     </div>
