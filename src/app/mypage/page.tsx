249d4a254d55f0fb69041bda9e346d6fe4e6c40c
AnYoonKyoung <anky0425@naver.com>
2024-08-09 23:22:36 +0900

    refactor(develop):페이지네이션 추가 및 로딩 상태 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -6,20 +6,31 @@ import Card from "../../components/Card/card";
 import { storeListHandler } from "../../hook/storeListHandler";
 export default function Page() {
   const { data: session } = useSession();
+
+  const [pagination, setPagination] = useState({
+    total: 0,
+    page: 1,
+    limit: 10,
+    totalPages: 0,
+  });
   const userCode = localStorage.getItem("userCode");
   const [cardData, setCardData] = useState([]);
 
   useEffect(() => {
     if (userCode) {
-      storeListHandler(userCode, setCardData);
+      storeListHandler(
+        userCode || null,
+        setCardData,
+        setPagination,
+        pagination
+      );
     }
   }, []);
 
-  console.log(session);
   return (
     <div className="justify-center mr-60 ml-60 mt-40">
       <div className="w-full h-48 border-2 border-gray-300">
-        <Image src="/img/Pide.png" alt="Logo" width={100} height={70} />
+        {/* <Image src="/img/Pide.png" alt="Logo" width={100} height={70} /> */}
         {/* <div>{session.email}</div> */}
       </div>
       <div className="flex text-3xl">my tour List</div>
