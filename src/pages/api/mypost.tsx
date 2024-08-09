1b47d9f7deac99e5a269955ccc6347418395710e
AnYoonKyoung <anky0425@naver.com>
2024-08-09 23:17:32 +0900

    fix(develop):페이지네이션 기능 추가

--- src/pages/api/mypost.tsx
+++ src/pages/api/mypost.tsx
@@ -1,78 +1,14 @@
-// import dbConnect from "../../lib/mongodb";
-// import { getToken } from "next-auth/jwt";
-// import protect from "../../lib/auth";
-// import storeArray from "../../lib/storeArray";
-
-// const handler = async (req, res) => {
-//   const { method } = req;
-
-//   await dbConnect();
-//   const token = await getToken({ req, secret: process.env.AUTH_CLIENT_SECRET });
-//   if (!token) {
-//     return res.status(401).json({ success: false, message: "Not authorized" });
-//   }
-
-//   switch (method) {
-//     case "POST":
-//       try {
-//         const roads = req.body; // 배열로 가정
-//         // 필드 유효성 검사
-//         for (const road of roads) {
-//           if (!road.name) {
-//             return res
-//               .status(400)
-//               .json({
-//                 success: false,
-//                 message: "Missing 'name' field in request body",
-//               });
-//           }
-//         }
-//         // 로그인한 사용자 정보 가져오기
-//         const user = req.user;
-//         // console.log(roads, user);
-//         // 사용자별 동적 모델 생성
-//         const Road = storeArray(user.name); // 예: email을 기반으로 컬렉션 생성
-//         const ro = roads;
-//         // 배열의 모든 항목을 데이터베이스에 저장
-
-//         const savedRoads = await Road.insertMany(roads);
-
-//         res.status(201).json({ success: true, data: savedRoads });
-//       } catch (error) {
-//         res.status(400).json({ success: false, message: error.message });
-//       }
-//       break;
-//     default:
-//       res.status(400).json({ success: false });
-//       break;
-//   }
-// };
-
-// export default handler;
-
 import dbConnect from "../../lib/mongodb";
-import { getSession } from "next-auth/react"; // next-auth에서 getSession 임포트
+import type { NextApiRequest, NextApiResponse } from "next";
 import Road from "../../lib/userRoad";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 
-const handler = async (req, res) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
   await dbConnect();
-  // const session = await getSession({ req }); // 세션 가져오기
-  // if (!session || !session.user) {
-  //   return res.status(401).json({ success: false, message: "Not authorized" });
-  // }
-  // console.log(session);
-  // const userCode = session?.user?.code || session?.user?.userCode; // userCode가 다른 경로에 있을 가능성 고려
-  // console.log("User Code:", userCode);
 
-  // if (!userCode) {
-  //   return res
-  //     .status(400)
-  //     .json({ success: false, message: "User code not found in session" });
-  // }
   switch (method) {
     case "POST":
       try {
@@ -105,14 +41,33 @@ const handler = async (req, res) => {
     case "GET":
       try {
         const { userCode } = req.query;
+        const page = parseInt(req.query.page) || 1; // 현재 페이지 (기본값 1)
+        const limit = parseInt(req.query.limit) || 10; // 한 페이지에 표시할 항목 수 (기본값 10)
+        const skip = (page - 1) * limit; // 페이지네이션 계산
 
         let filteredRoads;
-        if (userCode) {
-          filteredRoads = await Road.find({ userCode });
-        } else {
-          filteredRoads = await Road.find(); // Return all records if no userCode
-        }
-        res.status(200).json({ success: true, data: filteredRoads });
+        const countPromise = Road.countDocuments(userCode ? { userCode } : {});
+        const roadsPromise = Road.find(userCode ? { userCode } : {})
+          .skip(skip)
+          .limit(limit);
+
+        const [count, roads] = await Promise.all([countPromise, roadsPromise]);
+        // let filteredRoads;
+        // if (userCode) {
+        //   filteredRoads = await Road.find({ userCode });
+        // } else {
+        //   filteredRoads = await Road.find(); // Return all records if no userCode
+        // }
+        res.status(200).json({
+          success: true,
+          data: roads,
+          pagination: {
+            total: count,
+            page,
+            limit,
+            totalPages: Math.ceil(count / limit),
+          },
+        });
       } catch (error) {
         res.status(400).json({ success: false, message: error.message });
       }
