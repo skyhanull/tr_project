ee5ea2edcfac6f8de1342325c021699d28d114fc
AnYoonKyoung <anky0425@naver.com>
2024-08-09 23:21:48 +0900

    feat(develop):리스트 몽고구스 모델 추가

---
+++ src/lib/userRoad.tsx
@@ -0,0 +1,59 @@
+// // lib/dynamicModel.js
+// import mongoose from "mongoose";
+
+// const createDynamicModel = (username) => {
+//   const RoadSchema = new mongoose.Schema({
+//     address: {
+//       type: String,
+//       required: true,
+//     },
+//     filterChip: {
+//       type: String,
+//       required: true,
+//     },
+//     chip: {
+//       type: String,
+//       required: true,
+//     },
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     x: {
+//       type: String,
+//       required: true,
+//     },
+//     y: {
+//       type: String,
+//       required: true,
+//     },
+//   });
+
+//   return (
+//     mongoose.models[username] || mongoose.model(username, RoadSchema, username)
+//   );
+// };
+
+// export default createDynamicModel;
+import mongoose from "mongoose";
+
+const userRoadSchema = new mongoose.Schema({
+  userCode: { type: String, ref: "User" },
+  listName: { type: String },
+  date: { type: String },
+  roads: [
+    {
+      name: String,
+      code: String,
+      chip: String,
+      x: String,
+      y: String,
+      filterChip: String,
+      address: String,
+      // 추가적인 필드
+    },
+  ],
+});
+
+const Road = mongoose.models.Road || mongoose.model("Road", userRoadSchema);
+export default Road;
