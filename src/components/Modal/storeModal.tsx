6334b92173602c7276a8e9c2414093752e1979fa
AnYoonKyoung <anky0425@naver.com>
2024-08-11 02:39:01 +0900

    refactor(develop):api 호출 변경 및 ui 수정

--- src/components/Modal/storeModal.tsx
+++ src/components/Modal/storeModal.tsx
@@ -1,6 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, ChangeEvent } from "react";
 import axios from "axios";
+import Image from "next/image";
 import useDebounce from "@/hook/useDebounce";
+import SubmitButton from "../button/submitButton";
+// import { useImageUpload } from "@/hook/useFile";
+import ImgModal from "../Modal/ImgSelectModal";
 
 interface ModalProps {
   isOpen: boolean;
@@ -11,16 +15,21 @@ interface ModalProps {
 
 const Modal = ({ isOpen, onClose, directions, markerList }: ModalProps) => {
   const userCode = localStorage.getItem("userCode");
+  const [isImg, setIsImg] = useState(false);
+  const [uploadUrl, setUploadUrl] = useState<string | null>(null);
+  // const { imageFile, imagePreview, handleImageUpload, resetImage } =
+  //   useImageUpload();
+
   const [inputValue, setInputValue] = useState("");
   const debouncedListName = useDebounce(inputValue, 1000);
 
-  const handleSubmits = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmits = async () => {
+    // e.preventDefault();
 
     try {
       const res = await axios.post(
         "/api/mypost",
-        { markerList, userCode, listName: debouncedListName },
+        { markerList, userCode, listName: debouncedListName, image: uploadUrl },
         {
           headers: {
             "Content-Type": "application/json",
@@ -44,6 +53,15 @@ const Modal = ({ isOpen, onClose, directions, markerList }: ModalProps) => {
     onClose();
   };
 
+  const handleImageSelect = (url: string) => {
+    setUploadUrl(url);
+  };
+
+  const handleClose = () => {
+    onClose();
+    // resetImage();
+  };
+
   if (!isOpen) return null;
 
   return (
@@ -52,21 +70,33 @@ const Modal = ({ isOpen, onClose, directions, markerList }: ModalProps) => {
         <div className="p-4 border-b">
           <button
             className="text-gray-600 hover:text-gray-900"
-            onClick={handleSubmit}
+            onClick={handleClose}
           >
             &times;
           </button>
         </div>
         <div className="p-4">
-          <div className="flex flex-row">
-            <div className="">
-              <div className="bg-slate-500 h-32 w-32 my-2 mx-5 rounded-lg" />
+          <div className="flex flex-row justify-center items-center">
+            <div className="relative p-3" onClick={() => setIsImg(true)}>
+              {uploadUrl ? (
+                <Image
+                  src={uploadUrl}
+                  alt="Preview"
+                  className="h-32 w-32 rounded-lg object-cover"
+                  width={128}
+                  height={128}
+                />
+              ) : (
+                <div className="bg-slate-500 h-32 w-32 rounded-lg flex items-center justify-center cursor-pointer">
+                  <span className="text-white">이미지 선택</span>
+                </div>
+              )}
             </div>
-            <div className="p-3">
+            <div className="p-3 ml-3">
               <div className="flex flex-row items-center">
                 <span className="w-20 font-bold">북마크 추가</span>
               </div>
-              <div className="py-3 flex flex-row items-center">
+              <div className="py-4 flex flex-row items-center ">
                 <span className="w-12">이름</span>
                 <input
                   className="border-2 border-gray-300 rounded-md p-1"
@@ -79,19 +109,27 @@ const Modal = ({ isOpen, onClose, directions, markerList }: ModalProps) => {
               </div>
             </div>
           </div>
-          <div className="flex justify-end">
-            <button className="bg-red-300 p-3 rounded-lg" onClick={onClose}>
-              취소
-            </button>
-            <button
-              className="bg-red-300 p-3 rounded-lg"
-              onClick={handleSubmits}
-            >
-              추가
-            </button>
+          <div className="flex justify-end my-3">
+            <SubmitButton
+              clickHandler={handleClose}
+              name={"취소"}
+              state={false}
+            />
+            <SubmitButton
+              clickHandler={handleSubmits}
+              name={"추가"}
+              state={true}
+            />
           </div>
         </div>
       </div>
+      {isImg && (
+        <ImgModal
+          isOpen={isImg}
+          onClose={() => setIsImg(false)}
+          onSelectImage={handleImageSelect}
+        />
+      )}
     </div>
   );
 };
