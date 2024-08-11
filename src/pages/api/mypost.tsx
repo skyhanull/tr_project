72c8956b55e703c35653e888dcbb308e79a0c919
AnYoonKyoung <anky0425@naver.com>
2024-08-11 21:47:41 +0900

    feat(develop):공개/비공개 값 추가

--- src/pages/api/mypost.tsx
+++ src/pages/api/mypost.tsx
@@ -12,7 +12,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (method) {
     case "POST":
       try {
-        const { userCode, markerList, listName, image } = req.body;
+        const { userCode, markerList, listName, image, visibility } = req.body;
 
         // 각 도로 객체에 userCode 추가
         if (!userCode || !Array.isArray(markerList)) {
@@ -31,6 +31,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           date,
           listName,
           image,
+          visibility,
           roads: markerList, // `roads` 필드에 `markerList` 저장
         });
 
@@ -41,24 +42,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       break;
     case "GET":
       try {
-        const { userCode } = req.query;
+        const { userCode, status } = req.query;
         const page = parseInt(req.query.page) || 1; // 현재 페이지 (기본값 1)
         const limit = parseInt(req.query.limit) || 10; // 한 페이지에 표시할 항목 수 (기본값 10)
         const skip = (page - 1) * limit; // 페이지네이션 계산
 
-        let filteredRoads;
-        const countPromise = Road.countDocuments(userCode ? { userCode } : {});
-        const roadsPromise = Road.find(userCode ? { userCode } : {})
-          .skip(skip)
-          .limit(limit);
+        let filter: { userCode?: string; status?: string } = {};
+        if (userCode) {
+          filter.userCode = userCode;
+        }
+        if (status) {
+          filter.status = status; // Add status filtering if provided
+        }
+
+        const countPromise = Road.countDocuments(filter);
+        const roadsPromise = Road.find(filter).skip(skip).limit(limit);
 
         const [count, roads] = await Promise.all([countPromise, roadsPromise]);
-        // let filteredRoads;
-        // if (userCode) {
-        //   filteredRoads = await Road.find({ userCode });
-        // } else {
-        //   filteredRoads = await Road.find(); // Return all records if no userCode
-        // }
+
         res.status(200).json({
           success: true,
           data: roads,
