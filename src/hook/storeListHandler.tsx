3540ad56c8cb89184cc9ec4a8bd43a6ac87bc975
AnYoonKyoung <anky0425@naver.com>
2024-08-12 20:40:41 +0900

    feat(develop):페이지네이션 훅 생성

--- src/hook/storeListHandler.tsx
+++ src/hook/storeListHandler.tsx
@@ -1,24 +1,36 @@
 // utils/fetchMarkers.ts
 import axios from "axios";
 
+interface Pagination {
+  page: number;
+  limit: number;
+}
+
 export const storeListHandler = async (
   userCode: string | null,
-  setCardData: (data: any) => void
+  setCardData: (data: any) => void,
+  setPagination: (pagination: any) => void,
+  pagination: Pagination
 ) => {
   try {
-    // Prepare query parameters conditionally
-    const params: any = {};
-    if (userCode) {
-      params.userCode = userCode;
-    }
-
     // 서버로 GET 요청 보내기
     const response = await axios.get("/api/mypost", {
-      params, // Only include userCode if it is provided
+      params: {
+        userCode: userCode || undefined, // userCode가 있을 때만 포함
+        page: pagination.page,
+        limit: pagination.limit,
+      },
     });
 
-    console.log("Response:", response.data);
+    // 데이터를 상태에 저장
     setCardData(response.data.data);
+
+    // 페이지네이션 정보 업데이트
+    setPagination({
+      ...pagination,
+      total: response.data.pagination.total, // 총 데이터 수
+      totalPages: response.data.pagination.totalPages, // 총 페이지 수
+    });
   } catch (error) {
     console.error("Error fetching markers:", error);
   }
