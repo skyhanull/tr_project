affa5b9489d3a0c0525dc80c9ecb4fa866637ad3
AnYoonKyoung <anky0425@naver.com>
2024-08-14 00:40:14 +0900

    feat(develop):avata컴포넌트 생성

---
+++ src/components/img/avata.tsx
@@ -0,0 +1,21 @@
+import Avatar from "@mui/material/Avatar";
+import { red } from "@mui/material/colors";
+
+interface AvataImgProps {
+  img?: string; // Optional prop for image URL
+  name?: string; // Optional prop for user's name to display initial
+}
+
+const AvataImg = ({ img, name }: AvataImgProps) => {
+  return (
+    <Avatar
+      src={img} // If `img` is provided, use it for the Avatar
+      sx={{ bgcolor: !img ? red[500] : "transparent" }} // Set background color if no image
+      aria-label="recipe"
+    >
+      {!img && name?.charAt(0)} {/* Display the initial if no image */}
+    </Avatar>
+  );
+};
+
+export default AvataImg;
