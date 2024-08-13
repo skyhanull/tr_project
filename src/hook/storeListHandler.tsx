9ed737c812d7ae4c3c1cabfa2ae96dd68b5619b9
AnYoonKyoung <anky0425@naver.com>
2024-08-14 00:42:17 +0900

    fix(develop): 요소 추가

--- src/hook/storeListHandler.tsx
+++ src/hook/storeListHandler.tsx
@@ -10,15 +10,17 @@ export const storeListHandler = async (
   userCode: string | null,
   setCardData: (data: any) => void,
   setPagination: (pagination: any) => void,
-  pagination: Pagination
+  pagination: Pagination,
+  sort: string
 ) => {
   try {
     // 서버로 GET 요청 보내기
     const response = await axios.get("/api/mypost", {
       params: {
-        userCode: userCode || undefined, // userCode가 있을 때만 포함
+        userCode: userCode, // userCode가 있을 때만 포함
         page: pagination.page,
         limit: pagination.limit,
+        sortBy: sort,
       },
     });
 
