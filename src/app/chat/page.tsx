1ddd3808a5ae3c9ec6f6f795e97ace92a1a4e2d9
AnYoonKyoung <anky0425@naver.com>
2024-09-02 23:40:19 +0900

    fix(develop): 페이지 구조 변경

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -26,19 +26,34 @@
 // }
 "use client";
 import React, { useState } from "react";
-import ChatRoomList from "@/components/ChatRoomList";
-import ChatRoom from "@/components/ChatRoom";
+import ChatRoomList from "../../components/chatRoomList";
+import ChatRoom from "../../components/chatroom";
 
 export default function Chat() {
   const [selectedRoomId, setSelectedRoomId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div>
-      {selectedRoomId ? (
+    <div className=" mt-24 ml-20">
+      {/* {selectedRoomId ? (
         <ChatRoom roomId={selectedRoomId} />
       ) : (
         <ChatRoomList onSelectRoom={(roomId) => setSelectedRoomId(roomId)} />
-      )}
+      )} */}
+      <div>
+        <div className="flex  bg-slate-50 p-3 justify-end">
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="ml-4 bg-green-500 text-white p-2 rounded flex"
+          >
+            + ADD
+          </button>
+        </div>
+        <div className="flex">
+          <ChatRoomList onSelectRoom={(roomId) => setSelectedRoomId(roomId)} />
+          <ChatRoom roomId={selectedRoomId} />
+        </div>
+      </div>
     </div>
   );
 }
